40fea50c77f1afc67ffc83db4b1e31ee9738a9e7
AserDen <37746806+AserDen@users.noreply.github.com>
2026-01-21 08:14:43 +0200

    Create db.dart

---
+++ lib/data/db.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
+
+import 'tables.dart';
+
+part 'db.g.dart';
+
+@DriftDatabase(tables: [Faults, Notes])
+class AppDb extends _$AppDb {
+  AppDb() : super(_openConnection());
+
+  @override
+  int get schemaVersion => 1;
+}
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dir = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dir.path, 'alarm_matrix.sqlite'));
+    return NativeDatabase.createInBackground(file);
+  });
+}
